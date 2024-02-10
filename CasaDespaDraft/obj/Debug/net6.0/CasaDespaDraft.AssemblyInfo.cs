e5d57b692f57096d209e7898de84743fdfe03417
Jas Arangali <142953560+JArangali@users.noreply.github.com>
2024-02-10 13:09:28 +0800

    FAQs & Gallery

--- CasaDespaDraft/obj/Debug/net6.0/CasaDespaDraft.AssemblyInfo.cs
+++ CasaDespaDraft/obj/Debug/net6.0/CasaDespaDraft.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasaDespaDraft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6820cc2b616ef773dd3d52138022c4b7497d83e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasaDespaDraft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasaDespaDraft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
