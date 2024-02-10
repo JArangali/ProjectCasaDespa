e4f9698f78c93b1e11066f7eb3d9ce8bb0122064
Jas Arangali <142953560+JArangali@users.noreply.github.com>
2024-02-10 15:30:18 +0800

    Goldie Push hihi

---
+++ CasaDespaDraft/Controllers/BookingController.cs
@@ -0,0 +1,27 @@
+﻿using CasaDespaDraft.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CasaDespaDraft.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly ILogger<BookingController> _logger;
+
+        public BookingController(ILogger<BookingController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
