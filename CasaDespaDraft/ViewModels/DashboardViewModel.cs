8a06b607b994f6ee599d317fd4450e907f02d5ec
Jas Arangali <142953560+JArangali@users.noreply.github.com>
2024-02-22 11:47:05 +0800

    Dashboard Part 1

---
+++ CasaDespaDraft/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CasaDespaDraft.Models;
+
+namespace CasaDespaDraft.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public IEnumerable<Booking> Bookings { get; set; }
+        public IEnumerable<Booking> Accepted { get; set; }
+        public IEnumerable<Booking> Completed { get; set; }
+        public IEnumerable<Booking> Archive { get; set; }
+    }
+}
