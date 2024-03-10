2e957ed42ccf4836d0cfd6cffec7da3caf2de878
ltgranada <luisenrico.granada.cics@ust.edu.ph>
2024-03-10 15:20:24 +0800

    March 10 - Luis

---
+++ CasaDespaDraft/Models/DayDates.cs
@@ -0,0 +1,15 @@
+﻿using CasaDespaDraft.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CasaDespaDraft.Models
+{
+    public class DayDates
+    {
+        [Key]
+        public int dayId { get; set; }
+        public string? dayDate { get; set; }
+    }
+}
---
+++ CasaDespaDraft/Models/TTInDates.cs
@@ -0,0 +1,16 @@
+﻿using CasaDespaDraft.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CasaDespaDraft.Models
+{
+    public class TTInDates
+    {
+        [Key]
+        public int ttInId { get; set; }
+        public string? ttInDate { get; set; }
+
+    }
+}
